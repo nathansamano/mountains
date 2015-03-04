f03e26e8d54825929e03b5660df741f7f821cd4f
Nathan Samano <nsv9982@saintjoe.edu>
2015-03-04 12:01:41 -0500

    added capouch's files to update my repo

--- printDataRJ.js
+++ printDataRJ.js
@@ -1,4 +1,3 @@
-// Bring in the pieces
 load('calcFuncs.js');
 load('drawFuncs.js');
 load('TextCell.js');
@@ -6,7 +5,15 @@ load('UnderlinedCell.js');
 load('RTextCell.js');
 
 // Here's where you load the dataset
-load('mountains.js');
+// load('mountains.js');
+// load('houses.js');
+load('larder.js');
+
+// This code does date recognition so they can be right-justified
+//   You can turn it on and off
+var rightJustifyDates = false;
+// This regular expression contains logic-by-pattern to identify a date
+var dateReg = (rightJustifyDates?/^(\d{2}|\d)[.-/](\d|\d{2})[.-/]\d{2}$/:null);
 
 function dataTable(data) {
   var keys = Object.keys(data[0]);
@@ -17,13 +24,17 @@ function dataTable(data) {
     return keys.map(function(name) {
       var value = row[name];
       // This was changed:
-      if (typeof value == "number")
+      // Here is where new date type test will go
+      if ((typeof value == "number"))
         return new RTextCell(String(value));
       else
-        return new TextCell(String(value));
+        if(typeof value == "string" && value.match(dateReg))
+	  return new RTextCell(String(value));
+	else
+          return new TextCell(String(value));
     });
   });
   return [headers].concat(body);
 }
 
-print(drawTable(dataTable(MOUNTAINS)));
+print(drawTable(dataTable(LARDER_FILE)));
